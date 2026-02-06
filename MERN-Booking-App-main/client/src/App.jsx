84e49ea56b0498774e418d4cedf54cd448d831c1
Deepak Singh <deepak954807@gmail.com>
2026-02-06 12:19:31 +0530

    Fix missing closing Route tag in App component

--- MERN-Booking-App-main/client/src/App.jsx
+++ MERN-Booking-App-main/client/src/App.jsx
@@ -22,21 +22,24 @@ function App() {
 
   return (
     <UserContextProvider>
-      <Routes>
+  <Routes>
 
-      <Route path="/" element={<Layout/>}> <Route index element={<IndexPage />} /> 
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account" element={<ProfilePage />} />
-        <Route path="/account/places" element={<PlacesPage />} />
-        <Route path="/account/places/new" element={<PlacesFormPage />} /> 
-        <Route path="/account/places/:id" element={<PlacesFormPage />} /> 
-        <Route path="/place/:id" element={<PlacePage />} /> 
-        <Route path="/account/bookings" element={<BookingsPage />} />
-        <Route path="/account/bookings/:id" element={<BookingPage />} />
-        
-      </Routes>
-    </UserContextProvider>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/account" element={<ProfilePage />} />
+      <Route path="/account/places" element={<PlacesPage />} />
+      <Route path="/account/places/new" element={<PlacesFormPage />} />
+      <Route path="/account/places/:id" element={<PlacesFormPage />} />
+      <Route path="/place/:id" element={<PlacePage />} />
+      <Route path="/account/bookings" element={<BookingsPage />} />
+      <Route path="/account/bookings/:id" element={<BookingPage />} />
+    </Route>   {/* 👈 THIS was missing */}
+
+  </Routes>
+</UserContextProvider>
+
   )
 }
 
