6f7af0f06e185f542b0126c177336c67678b1557
Deepak Singh <deepak954807@gmail.com>
2026-02-06 12:15:05 +0530

    Refactor route definitions in App.jsx

--- MERN-Booking-App-main/client/src/App.jsx
+++ MERN-Booking-App-main/client/src/App.jsx
@@ -24,21 +24,16 @@ function App() {
     <UserContextProvider>
       <Routes>
 
-        <Route path="/" element={<Layout />}>
-  <Route index element={<IndexPage />} />
-  <Route path="login" element={<LoginPage />} />
-  <Route path="register" element={<RegisterPage />} />
-  <Route path="account" element={<ProfilePage />} />
-  <Route path="account/places" element={<PlacesPage />} />
-  <Route path="account/places/new" element={<PlacesFormPage />} />
-  <Route path="account/places/:id" element={<PlacesFormPage />} />
-  <Route path="place/:id" element={<PlacePage />} />
-  <Route path="account/bookings" element={<BookingsPage />} />
-  <Route path="account/bookings/:id" element={<BookingPage />} />
-</Route>
-
-
-        </Route>
+      <Route path="/" element={<Layout/>}> <Route index element={<IndexPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/account" element={<ProfilePage />} />
+        <Route path="/account/places" element={<PlacesPage />} />
+        <Route path="/account/places/new" element={<PlacesFormPage />} /> 
+        <Route path="/account/places/:id" element={<PlacesFormPage />} /> 
+        <Route path="/place/:id" element={<PlacePage />} /> 
+        <Route path="/account/bookings" element={<BookingsPage />} />
+        <Route path="/account/bookings/:id" element={<BookingPage />} />
         
       </Routes>
     </UserContextProvider>
